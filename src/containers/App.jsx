6c8a12a6093b626ac00a650077243e3f507f7e88
Jay Chung <xjkit54780@gmail.com>
2017-07-25 19:33:20 +0800

    Add new tab boilerplate

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
+import { Recording, Settings, Login, Finder } from 'containers';
 // materia-ui
 import AppBar from 'material-ui/AppBar';
 import FontIcon from 'material-ui/FontIcon';
@@ -8,9 +8,15 @@ import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNaviga
 import Paper from 'material-ui/Paper';
 // import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 
-const recentsIcon = <FontIcon className="material-icons">錄音</FontIcon>;
-const favoritesIcon = <FontIcon className="material-icons">檔案</FontIcon>;
-const settingsIcon = <FontIcon className="material-icons">設定</FontIcon>;
+const recentsIcon = (
+  <FontIcon className="material-icons">錄音</FontIcon>
+);
+const favoritesIcon = (
+  <FontIcon className="material-icons">檔案</FontIcon>
+);
+const settingsIcon = (
+  <FontIcon className="material-icons">設定</FontIcon>
+);
 
 const AppComponent = styled.div`
   display: flex;
@@ -34,6 +40,19 @@ class App extends Component {
 
   select = index => () => this.setState({ selectedIndex: index });
 
+  renderSelectedPage = () => {
+    switch(this.state.selectedIndex) {
+      case 0:
+        return <Recording />;
+      case 1:
+        return <Finder />;
+      case 2:
+        return <Settings />;
+      default:
+        return <Login />;
+    }
+  }
+
   render() {
     return (
       <AppComponent>
@@ -43,7 +62,7 @@ class App extends Component {
           />
         </HeaderComponent>
         <ContentComponent>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.renderSelectedPage()}
         </ContentComponent>
         <FooterComponent>
           <Paper>
---
+++ src/containers/Recording.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Recordings extends Component {
+  render() {
+    return (
+      <div className="h1">recording page</div>
+    );
+  }
+}
+
+export default Recordings;
